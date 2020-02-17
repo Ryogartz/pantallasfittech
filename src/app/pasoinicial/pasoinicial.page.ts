9ab66cb142411b47ab619252b56cf9940d86d2a8
brayan arias <bryanjose84@hotmail.com>
2020-02-17 16:23:14 -0400

    primer commmit

---
+++ src/app/pasoinicial/pasoinicial.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-pasoinicial',
+  templateUrl: './pasoinicial.page.html',
+  styleUrls: ['./pasoinicial.page.scss'],
+})
+export class PasoinicialPage implements OnInit {
+
+  constructor() {
+
+   }
+
+  ngOnInit() {
+
+  }
+
+
+}
